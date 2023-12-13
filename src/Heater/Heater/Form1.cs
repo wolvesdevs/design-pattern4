37f413cff203022dd561ba77064bf029b625691f
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-13 11:24:20 +0900

    refs #B04_べた書きでLow状態を実装する

--- src/Heater/Heater/Form1.cs
+++ src/Heater/Heater/Form1.cs
@@ -2,16 +2,32 @@ namespace Heater
 {
     public partial class Form1 : Form
     {
+        private enum Condition
+        {
+            OFF,
+            Low,
+            High
+        }
+        private Condition _condition = Condition.OFF;
+
         public Form1()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
-            DisplayLabel.Text = "";
+            DisplayLabel.Text = _condition.ToString();
         }
 
         private void UpButton_Click(object sender, EventArgs e)
         {
             string path = "heater.txt";
+
+            if (_condition == Condition.OFF)
+            {
+                var list = new List<string>();
+                list.Add("Low");
+                list.Add("50W");
+            }
+
             var list = new List<string>();
             list.Add("OFF");
             list.Add("0W");
