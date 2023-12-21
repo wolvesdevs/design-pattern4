ac6b2fc954c7282bcd03ed87eb41c3cc71402691
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-21 09:40:06 +0900

    refs #D04_機能拡張について考える

--- src/DDD/DDD/Form1.Designer.cs
+++ src/DDD/DDD/Form1.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Yu Gothic UI", 18F);
-            button1.Location = new Point(186, 121);
+            button1.Location = new Point(181, 147);
             button1.Name = "button1";
             button1.Size = new Size(432, 114);
             button1.TabIndex = 0;
@@ -42,19 +44,48 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Yu Gothic UI", 18F);
+            radioButton1.Location = new Point(159, 50);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(164, 52);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Normal";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Yu Gothic UI", 18F);
+            radioButton2.Location = new Point(375, 50);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(153, 52);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "大文字";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
     }
 }
