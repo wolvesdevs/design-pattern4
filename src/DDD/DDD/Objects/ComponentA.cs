c80d332d8936402d91d3cc16a4969f36a197e66e
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-21 09:46:37 +0900

    refs #D05_フラグでの解決_機能拡張を考える

--- src/DDD/DDD/Objects/ComponentA.cs
+++ src/DDD/DDD/Objects/ComponentA.cs
@@ -2,9 +2,23 @@
 {
     internal sealed class ComponentA : IComponent
     {
+        private bool _isUpper = false;
+
+        public ComponentA(bool isUpper)
+        {
+            _isUpper = isUpper;
+        }
+
         public string GetData()
         {
-            return File.ReadAllText("textA.txt");
+            var result = File.ReadAllText("textA.txt");
+            
+            if (_isUpper)
+            {
+                return result.ToUpper();
+            }
+
+            return result;
         }
     }
 }
