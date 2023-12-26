61006f47ef2e97514a1f11ab8cc82a549539f172
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-26 09:29:46 +0900

    refs #E02_通信処理部分の実装

--- src/DDDCom/DDDCom/Form1.cs
+++ src/DDDCom/DDDCom/Form1.cs
@@ -1,3 +1,5 @@
+using DDDCom.Messages;
+
 namespace DDDCom
 {
     public partial class Form1 : Form
@@ -5,11 +7,14 @@ namespace DDDCom
         public Form1()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void MeasureButton_Click(object sender, EventArgs e)
         {
-
+            var measureId = Convert.ToInt32(textBox1.Text);
+            var message = new MeasureMessage(measureId);
+            listBox1.Items.Add(string.Join("-", message.GetBytes()));
         }
     }
 }
