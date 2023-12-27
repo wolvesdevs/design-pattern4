ffddadd99fdb988de662bd9dcb47641458437678
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-27 09:14:04 +0900

    refs #F02_Device抽象クラスの作成

---
+++ src/Bridge/Bridge/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Bridge
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.White;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Yu Gothic UI", 18F);
+            label1.Location = new Point(68, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(298, 60);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 18F);
+            button1.Location = new Point(68, 296);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 69);
+            button1.TabIndex = 3;
+            button1.Text = "温度";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.White;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Yu Gothic UI", 18F);
+            label2.Location = new Point(68, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(298, 60);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.White;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Font = new Font("Yu Gothic UI", 18F);
+            label3.Location = new Point(68, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(298, 60);
+            label3.TabIndex = 5;
+            label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(912, 520);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ src/Bridge/Bridge/Form1.cs
@@ -0,0 +1,16 @@
+namespace Bridge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
