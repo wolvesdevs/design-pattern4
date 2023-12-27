6f7c67280e1c1d33100435ef89adfe58b442f330
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-27 15:53:50 +0900

    refs #F05_拡張での機能追加とその問題点

---
+++ src/Bridge/Bridge/Devices/BatteryDevice.cs
@@ -0,0 +1,22 @@
+﻿namespace Bridge.Devices
+{
+    public sealed class BatteryDevice : Device
+    {
+        private Random _random = new Random();
+
+        public string GetBatteryLeval()
+        {
+            return _random.Next(0, 100) + "%";
+        }
+
+        public override string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetMeasure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
