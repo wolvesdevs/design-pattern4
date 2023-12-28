ecea9f8b5b65d04039711fa8aa732c5b3ff7b849
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-28 10:19:30 +0900

    refs #F09_実装単位で切り出す

---
+++ src/Bridge/Bridge/Measures/WindMeasure.cs
@@ -0,0 +1,12 @@
+﻿namespace Bridge.Measures
+{
+    public sealed class WindMeasure : IMeasure
+    {
+        public string GetName() => "風力";
+
+        public string GetMeasure()
+        {
+            return File.ReadAllText("wind.txt") + "m/s";
+        }
+    }
+}
