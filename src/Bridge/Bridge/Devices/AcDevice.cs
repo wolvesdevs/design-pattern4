28c48d95478a655854e537b0d0d228fb1b91274b
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2023-12-30 15:34:36 +0900

    refs #F11_Deviceのサブクラスを実装する

---
+++ src/Bridge/Bridge/Devices/AcDevice.cs
@@ -0,0 +1,11 @@
+﻿using Bridge.Measures;
+
+namespace Bridge.Devices
+{
+    public sealed class AcDevice : Device
+    {
+        public AcDevice(IMeasure measure) : base(measure)
+        {
+        }
+    }
+}
---
+++ src/Bridge/Bridge/Devices/BatteryDevice.cs
@@ -0,0 +1,19 @@
+﻿using Bridge.Measures;
+using System;
+
+namespace Bridge.Devices
+{
+    public sealed class BatteryDevice : Device
+    {
+        private Random _random = new Random();
+
+        public BatteryDevice(IMeasure measure) : base(measure)
+        {
+        }
+
+        public string GetBatteryLeval()
+        {
+            return _random.Next(0, 100) + "%";
+        }
+    }
+}
